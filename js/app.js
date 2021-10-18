4634c7f96b42d0273bfe44ca989aa1ebdf113301
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-18 15:02:51 -0400

    enable handleclick function and write helper function to set up arrays for columns

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-/*-------------------------------- Constants --------------------------------*/
-// column 0: board[0, 6, 12, 18, 24]
-// column 1: board[1, 7, 13, 19, 25]
-// column 2: board[2, 8, 14, 20, 26]
-// column 3: board[3, 9, 15, 21, 27]
-// column 4: board[4, 10, 16, 22, 28]
-// column 5: board[5, 11, 17, 23, 29]
-
-/*---------------------------- Variables (state) ----------------------------*/
-
-let isGameOver, playerTurn, winConditions
-
 /*------------------------ Cached Element References ------------------------*/
 
 let board = document.getElementById('board')
@@ -18,6 +6,21 @@ let startMsg = document.getElementById('startMsg')
 let reset = document.getElementById('resetBtn')
 let topMsg = document.getElementById('msg')
 let circles = document.querySelectorAll('.circle')
+
+/*-------------------------------- Constants --------------------------------*/
+
+const column0 = [board[0], board[6], board[12], board[18], board[24]]
+const column1 = [board[1], board[7], board[13], board[19], board[25]]
+const column2 = [board[2], board[8], board[14], board[20], board[26]]
+const column3 = [board[3], board[9], board[15], board[21], board[27]]
+const column4 = [board[4], board[10], board[16], board[22], board[28]]
+const column5 = [board[5], board[11], board[17], board[23], board[29]]
+
+const columnsAll = [column0, column1, column2, column3, column4, column5]
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let isGameOver, playerTurn, winConditions, columnNumber, circleId, boardArr
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -31,20 +34,29 @@ init()
 function init(){
   startMsg.innerText = 'Start the game by picking a color.';
   start.style.display = 'block';
-  board = [
-    null, null, null, null, null, null,
-    null, null, null, null, null, null,
-    null, null, null, null, null, null,
-    null, null, null, null, null, null,
-    null, null, null, null, null, null,
+  boardArr = [
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
   ];
   isGameOver = false;
-  
   // clear pieces from the board
   // reset all values to null
   // make start buttons appear
-  render();
+  // render();
 }
+
+function render() {
+  if (boardArr === 0) {
+  } else if (boardArr === 1) {
+  }
+  else if (boardArr === -1) {
+  }
+}
+
+
 
 function handleStartClick(evt) {
   startMsg.innerHTML = `<h3>Choose the column you'd like to drop your piece down</h3>`;
@@ -57,51 +69,106 @@ function handleStartClick(evt) {
 }
 
 function handleClick(evt) {
-  console.log(evt.target.id);
-  // if (isGameOver === true) {
-  //   return
-  // }
+  columnNumber = parseInt(evt.target.id[2]);
+  let clNumArr = getColumn(columnNumber);
+  // circleId = evt.target.id.slice(4,6)
+  if (evt.target.id === 'board') {
+    return;
+  } else {
+    for (let i = clNumArr.length - 1; i >= 0; i--) {
+      if (boardArr[clNumArr[i]] === 0) {
+        boardArr[clNumArr[i]] = playerTurn;
+        playerTurn = playerTurn*-1
+        console.log(boardArr)
+        break;
+      }
+    }
+  }
+  render()
+}
+
+function getColumn(columnNumber) {
+  let arr = [columnNumber, columnNumber+6, columnNumber+12, columnNumber+18, columnNumber+24];
+  return arr;
+}
+// column 0: board[0, 6, 12, 18, 24]
+// column 1: board[1, 7, 13, 19, 25]
+// column 2: board[2, 8, 14, 20, 26]
+// column 3: board[3, 9, 15, 21, 27]
+// column 4: board[4, 10, 16, 22, 28]
+// column 5: board[5, 11, 17, 23, 29]
+
+    // if (boardArr[clNumArr[4]] === 0) {
+      //   boardArr[clNumArr[4]] = playerTurn;
+      // }
+
+
+// function render() {
+//   columnNumber
+// }
+
+// function handleClick(evt) {
+//   let idx = parseInt(evt.target.id);
+//   console.log(idx)
+//   if (isGameOver === true) {
+//     return;
+//   } else {
+//     let colArr = board[idx];
+//     console.log(colArr, board[idx])
+    // let rowIdx = colArr.indexOf(0);
+    // }
+    // if target id === 
+  // } 
+  // take the id of the circle +
+  // convert it to the column -> take id and then match it the column in the board array (?)
+  // find the lowest null circle in that column
+  // assign playerTurn value to that circle
+
+
   // let idx = parseInt(evt.target.id);
   // if (board[idx] === 1 || board[idx] === -1) {
   //   startMsg.innerText = `That circle's already been picked!`
   // } 
-  let columnSelected, circleSelected;
-  if (evt.target.id === 'board') {
-    topMsg.innerText = `Try again, you need to click inside the circle to drop your piece!`;
-    return;
-  } else {
-    circleSelected = parseInt(evt.target.id)
-  } if (circleSelected === 0 || circleSelected === 6 || circleSelected === 12 || circleSelected === 18 || circleSelected === 24) {
-    columnSelected = 0;
-  }
-  if (columnSelected === 0 && playerTurn === 1) {
-    document.getElementById('24').style.cssText = 'background-color: white; color: white; border-radius: 50%; width: 50px; height: 50px;'
-  }
-  
+//   render();
+
+
+// function render() {
+//   let columnSelected, circleSelected;
+//   if (evt.target.id === 'board') {
+//     topMsg.innerText = `Try again, you need to click inside the circle to drop your piece!`;
+//     return;
+//   } else {
+//     circleSelected = parseInt(evt.target.id)
+//   } if (circleSelected === 0 || circleSelected === 6 || circleSelected === 12 || circleSelected === 18 || circleSelected === 24) {
+//     columnSelected = 0;
+//   }
+//   if (columnSelected === 0 && playerTurn === 1) {
+//     document.getElementById('24').style.cssText = 'background-color: white; color: white; border-radius: 50%; width: 50px; height: 50px;'
+//   } else if (columnSelected === 0 && playerTurn === -1) {
+//     document.getElementById('24').classList.add('gray-circle')
+//     console.log(board)
+//   }
   // = playerTurn;
-    playerTurn = playerTurn * -1;
-  }
-  render()
-  // }
+//     playerTurn = playerTurn * -1;
 // }
 
 // function getColumn(idx) {
 //   let arr = 
 // }
 
-function render() {
-  circles.forEach((circle, idx) => {
-    if (board[idx] === 1) {
-      circle.innerText = 'X';
-    } else if (board[idx] === -1) {
-      circle.innerText = 'O';
-    } else {
-      circle.innerText = '';
-    }
-  }) 
-  topMsg.innerText = '';
+// function render() {
+//   circles.forEach((circle, idx) => {
+//     if (board[idx] === 1) {
+//       circle.innerText = 'X';
+//     } else if (board[idx] === -1) {
+//       circle.innerText = 'O';
+//     } else {
+//       circle.innerText = '';
+//     }
+//   }) 
+//   topMsg.innerText = '';
   // isGameOver()
-}
+// }
 
   // needs to put pieces at the bottom of each row --> if 0-5 are picked, then 24-29 are shown
 
