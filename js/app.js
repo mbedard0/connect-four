8e5d1fd760946d9deecf439993f14ba60b6c94af
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-20 00:00:32 -0400

    set up first diagnol generator

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ let circles = document.querySelectorAll('.circle')
 
 let isGameOver, playerTurn, boardArr
 
+let winner
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 start.addEventListener('click', handleStartClick)
@@ -50,6 +52,8 @@ function render() {
       circles[idx].classList.add('gray-circle');
     }
   });
+  if (winner === 1) {}
+  if (winner === -1) {}
   // pass function to end game, prevent further pieces from dropping
 }
 
@@ -68,22 +72,25 @@ function handleClick(evt) {
   if (isGameOver === true) {
     return;
   }
+  console.log(evt.target.id)
   let columnNumber = parseInt(evt.target.id[2]);
-  let rowNumber = parseInt(evt.target.id[4]);
+  let rowNumber;
+  let idxNum = parseInt(evt.target.id.slice(8,10));
   let clNumArr = getColumn(columnNumber);
   if (evt.target.id === 'board') {
     return;
   } else {
     for (let i = clNumArr.length - 1; i >= 0; i--) {
       if (boardArr[clNumArr[i]] === 0) {
+        rowNumber = i;
         boardArr[clNumArr[i]] = playerTurn;
-        playerTurn = playerTurn * -1
+        playerTurn = playerTurn * -1;
         console.log(boardArr)
         break;
       }
     }
   }
-  winConditions(rowNumber, columnNumber);
+  winner = winConditions(rowNumber, columnNumber, idxNum);
   render()
 }
 
@@ -97,57 +104,82 @@ function getRow(rowNumber) {
   return arr;
 }
 
-function columnArrValues(arr) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArray.push(board[arr[i]]);
+function getDiagonal1(idxNum, columnNumber, rowNumber) {
+  let arr = [];
+  let height = 5 - rowNumber;
+  let colToRight = 6 - columnNumber;
+  for (let i = 1; i <= Math.min(rowNumber,colToRight); i++) {
+    arr.push(idxNum - (6*i));
+  } 
+  for (let i = 0; i <= Math.min(height,columnNumber); i++) {
+    arr.push(idxNum + (6*i));
   }
-  return newArray;
-}
-
-function winConditions(rowNumber, columnNumber) {
-  if (vertWin(columnNumber) !== 0) {
-    isGameOver = true;
-  } else if (horizontalWin(rowNumber) === true) {
-    isGameOver = true;
-  } else if (diag1Win(arr) === true) {
-    isGameOver = true;
-  } else if ()
-}
-
-function vertWin(columnNumber) {
-  let arr1 = getColumn(columnNumber);
-  let arr2 = columnArrValues(arr1);
-// take column number
-// get indexes of all circles in that column
-// find the values of the circles at those indexes
-  let arr3 = createConsecutiveArray(arr2)
-  if (arr3.includes(4)) {
-    return 1;
-  } else if (arr3.includes(-4)) {
-    return -1;
-  } else {
-    return 0
-  }
-}
-
-function createConsecutiveArray(arr2) {
-  let consecArr = [];
-  let total = 0;
-  for (let i = 0; i < arr2.length; i++) {
-    if (i+1 === arr2.length) {
-      total += arr2[i];
-      consecArr.push(total);
-    } else if (arr2[i] === arr2[i+1]) {
-      total += arr2[i];
-    } else {
-      consecArr.push(total+arr2[i]);
-      total = 0;
-    }
-  }
-  return consecArr;
+  return arr.sort();
 }
 
 // [0, 0, 0, 0, 0, 1]
 // [1, 1, 1, 1, -1, -1]
 // [1, -1, 1, -1, 1, 1]
+
+// function lengthArrValues(arr) {
+//   let newArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     newArray.push(board[arr[i]]);
+//   }
+//   return newArray;
+// }
+
+// function winConditions(rowNumber, columnNumber, idxNum) {
+//   let vertWin = didWin(getColumn(columnNumber));
+//   let horizontalWin = didWin(getRow(rowNumber));
+//   let diagWin1 = didWin(getDiagonal1(idxNum))
+//   let diagWin2 = didWin(getDiagonal2(idxNum))
+//   if (vertWin !== 0) {
+//     isGameOver = true;
+//     return vertWin
+//   } else if (horizontalWin !== 0) {
+//     isGameOver = true;
+//     return horizontalWin
+//   } else if (diagWin1 !== 0) {
+//     isGameOver = true;
+//     return diagWin1
+//   } else if (diagWin2 !== 0) {
+//     isGameOver = true;
+//     return diagWin2
+//   } else {
+//     return 0
+//   }
+// }
+
+// function didWin(arr1) {
+//   let arr2 = lengthArrValues(arr1);
+//   let arr3 = createConsecutiveArray(arr2)
+//   if (arr3.includes(4)) {
+//     return 1;
+//   } else if (arr3.includes(-4)) {
+//     return -1;
+//   } else {
+//     return 0
+//   }
+// }
+
+// take column number
+// get indexes of all circles in that column
+// find the values of the circles at those indexes
+
+// function createConsecutiveArray(arr2) {
+//   let consecArr = [];
+//   let total = 0;
+//   for (let i = 0; i < arr2.length; i++) {
+//     if (i+1 === arr2.length) {
+//       total += arr2[i];
+//       consecArr.push(total);
+//     } else if (arr2[i] === arr2[i+1]) {
+//       total += arr2[i];
+//     } else {
+//       consecArr.push(total+arr2[i]);
+//       total = 0;
+//     }
+//   }
+//   return consecArr;
+// }
